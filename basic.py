ae8a8f37d83949e1e346af2b9e27276f6bb5f84a
Paul Mikesell <pmikesell@gmail.com>
2017-04-21 14:09:59 -0600

    Multi model training
    - Proper setting of default graph with multi-models
    - State passing vs not

--- basic.py
+++ basic.py
@@ -47,46 +47,52 @@ class TSModel(Model):
     def __init__(self, timesteps, feed_state=True, inputs=None):
         super().__init__()
 
+        self.graph = tf.Graph()
         sn = self.name
-        # If no input then build a placeholder expecing feed_dict
-        if inputs is None:
-            with tf.variable_scope(sn('input')):
-                self.add(tf.placeholder(tf.float32, (None, timesteps, 1)))
-        else:
-            self.output = inputs
+        self.timesteps = timesteps
 
-        with tf.variable_scope(sn('lstml_1')):
-            # XXX - fix state: https://www.tensorflow.org/tutorials/recurrent
-            # also: http://stackoverflow.com/questions/38241410/tensorflow-remember-lstm-state-for-next-batch-stateful-lstm
-            # also: tf.while?
+        with self.graph.as_default():
+            # If no input then build a placeholder expecing feed_dict
+            if inputs is None:
+                with tf.variable_scope(sn('input')):
+                    self.add(tf.placeholder(tf.float32, (None, timesteps, 1)))
+            else:
+                self.output = inputs
 
-            #             (lstm_cells,   2,     lstm_timesteps, 1)
-            #                (layers,   (c, h), timesteps,      outputs)
+            with tf.variable_scope(sn('lstml_1')):
+                # XXX - fix state: https://www.tensorflow.org/tutorials/recurrent
+                # also: http://stackoverflow.com/questions/38241410/tensorflow-remember-lstm-state-for-next-batch-stateful-lstm
+                # also: tf.while?
 
-            # enters with (batchsize, 1), exits with (batchsize, timesteps)
-            initial_state = None
-            if feed_state:
-                self.input_state = tf.placeholder(tf.float32, [timesteps, 2, lstm_batchsize, 1])
-                cht = tf.unstack(self.input_state, axis=0)
-                rnn_tuple_state = tuple([tf.contrib.rnn.LSTMStateTuple(cht[i][0], cht[i][1]) for i in range(timesteps)])
-                initial_state = rnn_tuple_state
+                #             (lstm_cells,   2,     lstm_timesteps, 1)
+                #                (layers,   (c, h), timesteps,      outputs)
 
-            # cells = [BasicLSTMCell(num_units=timesteps, state_is_tuple=True) for _ in range(timesteps)]
-            # multi_cell = MultiRNNCell(cells=cells, state_is_tuple=True)
-            cell = LSTMCell(1)
-            multi_cell = MultiRNNCell([cell]*timesteps)
+                # enters with (batchsize, 1), exits with (batchsize, timesteps)
+                initial_state = None
+                if feed_state:
+                    self.input_state = tf.placeholder(tf.float32, [timesteps, 2, lstm_batchsize, 1])
+                    cht = tf.unstack(self.input_state, axis=0)
+                    rnn_tuple_state = tuple([tf.contrib.rnn.LSTMStateTuple(cht[i][0], cht[i][1]) for i in range(timesteps)])
+                    initial_state = rnn_tuple_state
 
-            outputs, state = tf.nn.dynamic_rnn(cell=multi_cell, inputs=self.output,
-                                               dtype=tf.float32, initial_state=initial_state)
-            self.add(outputs)
-            self.state = state
+                # cells = [BasicLSTMCell(num_units=timesteps, state_is_tuple=True) for _ in range(timesteps)]
+                # multi_cell = MultiRNNCell(cells=cells, state_is_tuple=True)
+                cell = LSTMCell(1)
+                multi_cell = MultiRNNCell([cell]*timesteps)
+                if initial_state is None:
+                    initial_state = multi_cell.zero_state(1, tf.float32)
 
-            # Now make the training bits
-            self.labels = tf.placeholder(tf.float32, [lstm_batchsize, lstm_timesteps, 1], name='labels')
-            self.loss = tf.losses.mean_squared_error(self.output, self.labels)
-            opt = tf.train.AdamOptimizer(learning_rate=1e-4)
-            self.global_step = tf.contrib.framework.get_or_create_global_step()
-            self.train_opt = opt.minimize(self.loss, global_step=self.global_step)
+                outputs, state = tf.nn.dynamic_rnn(cell=multi_cell, inputs=self.output,
+                                                   dtype=tf.float32, initial_state=initial_state)
+                self.add(outputs)
+                self.state = state
+
+                # Now make the training bits
+                self.labels = tf.placeholder(tf.float32, [lstm_batchsize, timesteps, 1], name='labels')
+                self.loss = tf.losses.mean_squared_error(self.output, self.labels)
+                opt = tf.train.AdamOptimizer(learning_rate=1e-4)
+                self.global_step = tf.contrib.framework.get_or_create_global_step()
+                self.train_opt = opt.minimize(self.loss, global_step=self.global_step)
 
     def __repr__(self):
         out = super().__repr__()
@@ -121,37 +127,40 @@ def get_sess_config():
 def state_train(m, epochs):
 
     rvh = ReturnValuesHook(tensors={'step': m.global_step, 'loss': m.loss}, every_n_iter=1)
-    hooks = [tf.train.StopAtStepHook(last_step=10), rvh, ]
+    hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh, ]
 
-    state = np.zeros((lstm_timesteps, 2, lstm_batchsize, 1))
-    with tf.train.SingularMonitoredSession(hooks=hooks, config=get_sess_config()) as sess:
-        g = SinGen(timesteps=lstm_timesteps)
-        while not sess.should_stop():
-            x, y = g.batch()
-            (_, state) = sess.run([m.train_opt, m.state], feed_dict={m.input: x, m.labels: y, m.input_state: state})
+    with m.graph.as_default():
+        state = np.zeros((m.timesteps, 2, lstm_batchsize, 1))
+        with tf.train.SingularMonitoredSession(hooks=hooks, config=get_sess_config()) as sess:
+            g = SinGen(timesteps=m.timesteps)
+            while not sess.should_stop():
+                x, y = g.batch()
+                (_, state) = sess.run([m.train_opt, m.state], feed_dict={m.input: x, m.labels: y, m.input_state: state})
 
     return rvh.get_losses()
 
 
 def nostate_train(m, epochs):
-    rvh = ReturnValuesHook(tensors={'step': m.global_step, 'loss': m.loss}, every_n_iter=1)
-    hooks = [tf.train.StopAtStepHook(last_step=10), rvh, ]
 
-    with tf.train.SingularMonitoredSession(hooks=hooks, config=get_sess_config()) as sess:
-        g = SinGen(timesteps=lstm_timesteps)
-        while not sess.should_stop():
-            x, y = g.batch()
-            sess.run([m.train_opt], feed_dict={m.input: x, m.labels: y})
+    rvh = ReturnValuesHook(tensors={'step': m.global_step, 'loss': m.loss}, every_n_iter=1)
+    hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh, ]
+
+    with m.graph.as_default():
+        with tf.train.SingularMonitoredSession(hooks=hooks, config=get_sess_config()) as sess:
+            g = SinGen(timesteps=m.timesteps)
+            while not sess.should_stop():
+                x, y = g.batch()
+                sess.run([m.train_opt], feed_dict={m.input: x, m.labels: y})
 
     return rvh.get_losses()
 
 
 def main(_):
     m = TSModel(timesteps=lstm_timesteps)
-    m2 = TSModel(timesteps=lstm_timesteps, feed_state=False)
+    print(state_train(m, 5))
 
-    print(state_train(m, 10))
-    print(nostate_train(m2, 10))
+    m2 = TSModel(timesteps=lstm_timesteps, feed_state=False)
+    print(nostate_train(m2, 5))
 
 if __name__ == '__main__':
     tf.app.run()
