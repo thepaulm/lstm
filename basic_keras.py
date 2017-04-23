980649439c6099fa2fdcb03c94ea9d22b0694b48
Paul Mikesell <pmikesell@gmail.com>
2017-04-23 15:24:13 -0600

    Keras version works - I think I can use this to fix tensorflow.

---
+++ basic_keras.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers import TimeDistributed, Dense
+from keras.optimizers import Adam
+from singen import SinGen
+
+
+lstm_timesteps = 22  # lstm timesteps is how big to train on
+lstm_batchsize = 1
+lstm_units = 64
+
+
+class TSModel(object):
+    def __init__(self, timesteps):
+        self.m = Sequential()
+        # You can add two of these later
+        self.m.add(LSTM(lstm_units, return_sequences=True,
+                        input_shape=(timesteps, 1)))
+        self.m.add(TimeDistributed(Dense(1)))
+        self.m.compile(loss='mean_squared_error', optimizer=Adam())
+
+
+def train(m, epochs, lr):
+    m.m.optimizer.lr = lr
+    g = SinGen(timesteps=lstm_timesteps)
+    for i in range(epochs):
+        print('------------------------------------------')
+        print(i)
+        print('------------------------------------------')
+        x, y = g.batch()
+        m.m.fit(x, y, batch_size=1, epochs=10)
+
+
+def main():
+    m = TSModel(timesteps=lstm_timesteps)
+    train(m, 48, 1e-3)
+    train(m, 22, 1e-4)
+
+    m.m.save_weights('keras_lstm.h5')
+
+if __name__ == '__main__':
+    main()
