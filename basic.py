7f5790e4f1ed36725d8bfb1fd797508749a438fe
Paul Mikesell <pmikesell@gmail.com>
2017-04-23 17:00:27 -0600

    Basic with new model superclass and batching from the singen

--- basic.py
+++ basic.py
@@ -12,7 +12,7 @@ from singen import SinGen
 # lstm_units = 3  # lstm units decides how many outputs
 # lstm_cells = 4   # lstm cells is how many cells in the state
 lstm_timesteps = 22  # lstm timesteps is how big to train on
-lstm_batchsize = 1
+lstm_batchsize = 128
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -20,49 +20,45 @@ tf.logging.set_verbosity(tf.logging.INFO)
 class TSModel(Model):
     '''Basic timeseries tensorflow lstm model'''
 
-    def __init__(self, timesteps, feed_state=True, inputs=None):
+    def __init__(self, timesteps, feed_state=True):
         super().__init__()
-
-        self.graph = tf.Graph()
-        sn = self.name
         self.timesteps = timesteps
+        self.feed_state = feed_state
+        self._build(self.build)
 
-        with self.graph.as_default():
-            # If no input then build a placeholder expecing feed_dict
-            if inputs is None:
-                with tf.variable_scope(sn('input')):
-                    self.add(tf.placeholder(tf.float32, (None, timesteps, 1)))
-            else:
-                self.output = inputs
+    def build(self):
+        # If no input then build a placeholder expecing feed_dict
+        with tf.variable_scope('input'):
+            self.add(tf.placeholder(tf.float32, (None, self.timesteps, 1)))
 
-            with tf.variable_scope(sn('lstml_1')):
-                # XXX - fix state: https://www.tensorflow.org/tutorials/recurrent
+        with tf.variable_scope('lstml_1'):
+            # XXX - fix state: https://www.tensorflow.org/tutorials/recurrent
 
-                initial_state = None
-                if feed_state:
-                    self.input_state = tf.placeholder(tf.float32, [timesteps, 2, lstm_batchsize, 1])
-                    cht = tf.unstack(self.input_state, axis=0)
-                    rnn_tuple_state = tuple([tf.contrib.rnn.LSTMStateTuple(cht[i][0], cht[i][1]) for i in range(timesteps)])
-                    initial_state = rnn_tuple_state
+            initial_state = None
+            if self.feed_state:
+                self.input_state = tf.placeholder(tf.float32, [self.timesteps, 2, lstm_batchsize, 1])
+                cht = tf.unstack(self.input_state, axis=0)
+                rnn_tuple_state = tuple([tf.contrib.rnn.LSTMStateTuple(cht[i][0], cht[i][1]) for i in range(self.timesteps)])
+                initial_state = rnn_tuple_state
 
-                # cells = [BasicLSTMCell(num_units=timesteps, state_is_tuple=True) for _ in range(timesteps)]
-                # multi_cell = MultiRNNCell(cells=cells, state_is_tuple=True)
-                cells = [BasicLSTMCell(1) for _ in range(timesteps)]
-                multi_cell = MultiRNNCell(cells)
-                # if initial_state is None:
-                #     initial_state = multi_cell.zero_state(1, tf.float32)
+            # cells = [BasicLSTMCell(num_units=timesteps, state_is_tuple=True) for _ in range(timesteps)]
+            # multi_cell = MultiRNNCell(cells=cells, state_is_tuple=True)
+            cells = [BasicLSTMCell(1) for _ in range(self.timesteps)]
+            multi_cell = MultiRNNCell(cells)
+            # if initial_state is None:
+            #     initial_state = multi_cell.zero_state(1, tf.float32)
 
-                outputs, state = tf.nn.dynamic_rnn(cell=multi_cell, inputs=self.output,
-                                                   dtype=tf.float32, initial_state=initial_state)
-                self.add(outputs)
-                self.state = state
+            outputs, state = tf.nn.dynamic_rnn(cell=multi_cell, inputs=self.output,
+                                               dtype=tf.float32, initial_state=initial_state)
+            self.add(outputs)
+            self.state = state
 
-                # Now make the training bits
-                self.labels = tf.placeholder(tf.float32, [lstm_batchsize, timesteps, 1], name='labels')
-                self.loss = tf.losses.mean_squared_error(self.output, self.labels)
-                opt = tf.train.AdamOptimizer(learning_rate=1e-4)
-                self.global_step = tf.contrib.framework.get_or_create_global_step()
-                self.train_opt = opt.minimize(self.loss, global_step=self.global_step)
+            # Now make the training bits
+            self.labels = tf.placeholder(tf.float32, [lstm_batchsize, self.timesteps, 1], name='labels')
+            self.loss = tf.losses.mean_squared_error(self.output, self.labels)
+            opt = tf.train.AdamOptimizer(learning_rate=1e-3)
+            self.global_step = tf.contrib.framework.get_or_create_global_step()
+            self.train_opt = opt.minimize(self.loss, global_step=self.global_step)
 
     def __repr__(self):
         out = super().__repr__()
@@ -95,11 +91,19 @@ def get_sess_config():
     return config
 
 
-def state_train(m, epochs, log_every=1):
+def get_rvh(m, log_every, log_predictions):
+    log_map = {'step': m.global_step, 'loss': m.loss}
+    if log_predictions:
+        log_map.update({'label': m.labels, 'output': m.output})
+    rvh = ReturnValuesHook(tensors=log_map, every_n_iter=log_every)
+    return rvh
 
-    rvh = ReturnValuesHook(tensors={'step': m.global_step, 'loss': m.loss,
-                                    'label': m.labels, 'output': m.output}, every_n_iter=log_every)
-    hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh, ]
+
+def state_train(m, epochs, log_every=1, log_predictions=False):
+
+    rvh = get_rvh(m, log_every, log_predictions)
+    # hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh]
+    hooks = [rvh]
 
     with m.graph.as_default():
         state = np.zeros((m.timesteps, 2, lstm_batchsize, 1))
@@ -112,18 +116,20 @@ def state_train(m, epochs, log_every=1):
     return rvh.get_losses()
 
 
-def nostate_train(m, epochs, log_every=1):
+def nostate_train(m, epochs, log_every=1, log_predictions=False):
 
-    rvh = ReturnValuesHook(tensors={'step': m.global_step, 'loss': m.loss,
-                                    'label': m.labels, 'output': m.output}, every_n_iter=log_every)
-    hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh, ]
+    rvh = get_rvh(m, log_every, log_predictions)
+    # hooks = [tf.train.StopAtStepHook(last_step=epochs), rvh, ]
+    hooks = [rvh]
 
     with m.graph.as_default():
         with tf.train.SingularMonitoredSession(hooks=hooks, config=get_sess_config()) as sess:
-            g = SinGen(timesteps=m.timesteps)
+            g = SinGen(timesteps=m.timesteps, batchsize=lstm_batchsize)
             while not sess.should_stop():
                 x, y = g.batch()
-                sess.run([m.train_opt], feed_dict={m.input: x, m.labels: y})
+                for _ in range(10):
+                    sess.run([m.train_opt], feed_dict={m.input: x, m.labels: y})
+                print("10 epochs")
 
     return rvh.get_losses()
 
@@ -138,7 +144,7 @@ def compare_models():
 
 def train_two():
     m2 = TSModel(timesteps=lstm_timesteps, feed_state=False)
-    print(nostate_train(m2, 500, log_every=20))
+    print(nostate_train(m2, 48, log_every=1))
 
 
 def main(_):
--- model/m.py
+++ model/m.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 
+import tensorflow as tf
+
 
 class Model(object):
     '''
     Base Model for building tensorflow graphs.
 
-    Example usage:
-    class AModel(Model):
-        def __init__(self):
-            super().__init__()
-            with tf.variable_scope(self.name('A_input')):
-                self.add(tf.placeholder(tf.float32, (None, ) + img_size + (3,)))
-
-            with tf.variable_scope(self.name('A_conv_1')):
-                self.add(tf.layers.conv2d(self.output, 64, (2, 2), strides=(2, 2),
-                         padding='valid', activation=tf.nn.relu))
-                self.add(tf.nn.max_pool(self.output, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                                        padding='VALID'))
-
+    Example usage: TBD
     '''
     def __init__(self):
         self.layers = []
         self.input = None
         self.output = None
         self.id = hex(id(self))
+
+    def _build(self, build_fn):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            with tf.variable_scope(str(self.id)):
+                build_fn()
 
     def add(self, l):
         '''
