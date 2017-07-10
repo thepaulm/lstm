c7ff783e3d679c9314acc421250f7e2d49ad1b7d
Paul Mikesell <pmikesell@gmail.com>
2017-07-09 20:14:59 -0700

    Convert tf keras to be more like pycharm

--- basic_keras_tf.py
+++ basic_keras_tf.py
@@ -3,31 +3,34 @@
 import argparse
 from tensorflow.contrib.keras.api.keras.models import Sequential
 from tensorflow.contrib.keras.api.keras.layers import LSTM
+from tensorflow.contrib.keras.api.keras.layers import Reshape
 from tensorflow.contrib.keras.api.keras.layers import Dense
 from tensorflow.contrib.keras.python.keras.layers.wrappers import TimeDistributed
 from tensorflow.contrib.keras.api.keras.optimizers import Adam
 from tensorflow.contrib.keras.api.keras.callbacks import TensorBoard
 from singen import SinGen
 
+_ = Dense
+_ = TimeDistributed
 
-lstm_timesteps = 22  # lstm timesteps is how big to train on
+
+lstm_timesteps = 100  # lstm timesteps is how big to train on
 lstm_batchsize = 128
 lstm_units = 64
 
 
 class TSModel(object):
-    def __init__(self, timesteps, batchsize, stateful=False):
+    def __init__(self, timesteps, batchsize):
         self.m = Sequential()
-        # You can add two of these later
-        if stateful:
-            bis = (batchsize, timesteps, 1)
-            self.m.add(LSTM(lstm_units, return_sequences=True, stateful=stateful,
-                            batch_input_shape=bis, input_shape=(timesteps, 1)))
-        else:
-            self.m.add(LSTM(lstm_units, return_sequences=True, stateful=False,
-                            input_shape=(timesteps, 1)))
-        self.m.add(TimeDistributed(Dense(1)))
+
+        self.m.add(LSTM(units=lstm_units, return_sequences=True, stateful=False,
+                        input_shape=(timesteps, 1)))
+        self.m.add(LSTM(units=1, return_sequences=True, stateful=False))
+        self.m.add(Reshape((timesteps, 1)))
+
         self.m.compile(loss='mean_squared_error', optimizer=Adam())
+
+        print(self.m.summary())
 
 
 def train(m, epochs, lr, batchsize, tensorboard):
@@ -43,6 +46,7 @@ def train(m, epochs, lr, batchsize, tensorboard):
         print(i)
         print('------------------------------------------')
         x, y = g.batch()
+        print("x shape: ", x.shape)
         m.m.fit(x, y, batch_size=lstm_batchsize, epochs=10,
                 callbacks=callbacks
                 )
@@ -58,8 +62,7 @@ def get_args():
 def main():
     args = get_args()
     m = TSModel(timesteps=lstm_timesteps, batchsize=lstm_batchsize)
-    train(m, 256, 1e-3, lstm_batchsize, args.tensorboard)
-    train(m, 64, 1e-4, lstm_batchsize, args.tensorboard)
+    train(m, 384, 1e-3, lstm_batchsize, args.tensorboard)
 
     if args.save is not None:
         m.m.save_weights(args.save)
