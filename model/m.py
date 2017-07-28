813093c6688291f8866e0686eb28508ac4b50b74
Paul Mikesell <pmikesell@gmail.com>
2017-07-27 20:27:16 -0700

    license and default epochs

--- model/m.py
+++ model/m.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# Copyright 2017 Paul Mikesell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import tensorflow as tf
 
 
@@ -67,7 +81,7 @@ class Model(object):
 
         return self.session
 
-    def fit(self, x, y, epochs, verbose=True, log_every=1):
+    def fit(self, x, y, epochs=1, verbose=True, log_every=1):
         '''fit to fix predictions to labels.'''
 
         losses = []
